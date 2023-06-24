ca9c4a464c9ddea8819f9d6f9b1f65d1a2a273d2
veralara.bernhard <veralara.bernhard@uzh.ch>
2023-06-24 16:23:00 +0200

    Update plot

--- plots/plot_magma_results.py
+++ plots/plot_magma_results.py
@@ -5,68 +5,88 @@ import numpy as np
 from bokeh.palettes import Category20c
 from matplotlib import style
 
+def scale_array(df, x, y):
+    min_val = df.min()
+    max_val = df.max()
+    scaled_df = ((y - x) * (df - min_val) / (max_val - min_val)) + x
+    return scaled_df
+
 style.use('ggplot')
 my_palette = Category20c[20]
 
-with open('eval_vibrant_mountain.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    eval_losses = []
-    steps = []
-    for row in reader:
-        eval_losses.append(float(row['vibrant-mountain-58 - eval/loss']))
-        steps.append(int(row['Step']))
-        
-        
-    # Draw from a normal distribution to create vector lenth of eval_losses
-    some_random = np.random.normal(size=len(eval_losses))
-    # Add an offset to the random vector
-    some_random = some_random + 4
-    
-    some_random2 = np.random.normal(size=len(eval_losses)) + 4
-    some_random3 = np.random.normal(size=len(eval_losses)) + 4
+eval = pd.read_csv('eval_vibrant_mountain.csv')
+metrics = pdf   = pd.read_csv('metrics.csv')
+ 
+eval_loss = eval['vibrant-mountain-58 - eval/loss']
+eval_color = my_palette[0]        
+plot = sns.lineplot(
+    x=eval['Step'], 
+    y=eval_loss, 
+    color=eval_color,
+    label='Eval Loss'
+    )
 
+# Add dots for each data point
+sns.scatterplot(
+    x=eval['Step'],
+    y=eval_loss,
+    color=eval_color,
+    s=15
+)
+
+smoothed_data = pd.Series(eval['vibrant-mountain-58 - eval/loss']).rolling(10, min_periods=1, center=True).mean()
+smooth_color = my_palette[1]
+plot = sns.lineplot(
+    x=eval['Step'],
+    y=smoothed_data, 
+    linestyle='dashed', 
+    color=smooth_color, 
+    label='Smoothed Eval Loss'
+)
+
+metrics_labels = ['mean_bleu', 'mean_bertscore', 'mean_sembscore', 'mean_radgraphcombined', 'mean_cxrmetric']
+metrics_std_labels = ['std_bleu', 'std_bertscore', 'std_sembscore', 'std_radgraphcombined', 'std_cxrmetric']
+metrics_colors = [my_palette[4], my_palette[5], my_palette[8], my_palette[9], my_palette[10]]
+metrics_scales = [3, 3.5, 4, 4.5, 5, 5.5]
+metrics_labels_legend = ['Mean Bleu', 'Mean Bert Score', 'Mean Semb Score', 'Mean Radgraph', 'Mean CXR']
+
+for i, metric_label in enumerate(metrics_labels):
     plot = sns.lineplot(
-        x=steps, 
-        y=eval_losses, 
-        color=my_palette[0],
-        label='Eval Loss'
-        )
-    plot = sns.lineplot(
-        x=steps, 
-        y=pd.Series(eval_losses).rolling(10, min_periods=1, center=True).mean(), linestyle='dashed', 
-        color=my_palette[1], 
-        label='Smoothed Eval Loss'
+        x=metrics['step'],
+        y=scale_array(metrics[metric_label], metrics_scales[i], metrics_scales[i+1]),
+        color=metrics_colors[i],
+        label=metrics_labels_legend[i]
     )
     
-    plot = sns.lineplot(
-        x=steps, 
-        y=some_random, 
-        color=my_palette[4], 
-        label='Random')
-    
-    plot = sns.lineplot(
-        x=steps,
-        y=some_random2,
-        color = my_palette[5],
-        label = 'Random2'
+    plot.fill_between(
+        metrics['step'],
+        scale_array(metrics[metric_label] - metrics[metrics_std_labels[i]], metrics_scales[i], metrics_scales[i+1]),
+        scale_array(metrics[metric_label] + metrics[metrics_std_labels[i]], metrics_scales[i], metrics_scales[i+1]),
+        alpha=0.5,
+        color=metrics_colors[i]
     )
     
-    plot = sns.lineplot(
-        x=steps,
-        y=some_random3,
-        color = my_palette[6],
-        label = 'Random3'
+    # Add dots for each data point
+    sns.scatterplot(
+        x=metrics['step'],
+        y=scale_array(metrics[metric_label], metrics_scales[i], metrics_scales[i+1]),
+        color=metrics_colors[i],
+        s=15
     )
-    
-    # X-label
-    plot.set_xlabel('Steps')
-    # Y-label
-    plot.set_ylabel('Eval Loss')
-    
-    # Title
-    plot.set_title('Evaluation Loss vs Evaluation Metrics Trends')
-    
-            
-    # show plot
-    plot.figure.savefig('eval_vibrant_mountain.png')
+
+# Move the legend box to the right outside of the plot
+plot.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0.)
+
+
+# X-label
+plot.set_xlabel('Steps')
+# Y-label
+plot.set_ylabel('Eval Loss')
+
+# Title
+plot.set_title('Evaluation Loss vs Evaluation Metrics Trends')
+
+        
+# show plot
+plot.figure.savefig('eval_vibrant_mountain.png', bbox_inches='tight')
 
