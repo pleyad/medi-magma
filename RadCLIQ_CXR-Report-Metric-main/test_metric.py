9cc0879568cff48ba016b0c632ce887a76c799e3
Niclas Bodenmann <niclaslinus.bodenmann@uzh.ch>
2023-06-30 09:25:21 +0200

    Delete RadCliQ

--- RadCLIQ_CXR-Report-Metric-main/test_metric.py
+++
@@ -1,9 +0,0 @@
-import config
-from CXRMetric.run_eval import calc_metric
-
-gt_reports = config.GT_REPORTS
-predicted_reports = config.PREDICTED_REPORTS
-out_file = config.OUT_FILE
-
-if __name__ == "__main__":
-    calc_metric(gt_reports, predicted_reports, out_file)
